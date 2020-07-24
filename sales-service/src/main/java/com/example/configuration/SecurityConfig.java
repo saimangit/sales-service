fe3237c082192700e4d159e8f7066e884a2bcf2d
cognizantjava12 <cognizantjava12@cogjava137.iiht.tech>
2020-07-24 20:18:39 +0530

    24-07

--- sales-service/src/main/java/com/example/configuration/SecurityConfig.java
+++ sales-service/src/main/java/com/example/configuration/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.configuration;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 
@@ -19,6 +22,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		auth.inMemoryAuthentication().withUser("saiman").password("saiman").roles("USER").and().
 		withUser("testman").password("testman").roles(ADMIN);
 	}
+	
+	
+	@Bean
+	public PasswordEncoder getPass(){
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
 	
 	
 	
